97b70a3b2c75302cf727af5afe775fd27c67b79e
yuriy-jk <61912481+yuriy-jk@users.noreply.github.com>
2020-03-17 20:28:15 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,8 +1,3 @@
-server {
-    listen 80;
-    location / {
-        proxy_pass http://127.0.0.1:8000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    }
-  }
+pythonpath = '/home/box/web'
+bind = '0.0.0.0:8080'
+workers = 4
