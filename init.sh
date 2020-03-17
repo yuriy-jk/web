a7f9e59b1d67fa2e93f02946d96992e1541e07db
yuriy-jk <61912481+yuriy-jk@users.noreply.github.com>
2020-03-17 15:44:25 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
