408d95550694686f2e0e980cc36e50d6e5511904
yuriy-jk <61912481+yuriy-jk@users.noreply.github.com>
2020-03-17 21:05:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_hello
