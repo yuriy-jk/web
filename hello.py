f89897bcbf6ac59829a5631a9aabb75f3f25dccc
yuriy-jk <complete@bk.ru>
2020-03-17 14:28:16 +0300

    hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def wsgi_hello(environ, start_response):
     status = '200 OK'
-    headers = [('text/plain')]
+    headers = [('Content-type', 'text/plain')]
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
     return body
