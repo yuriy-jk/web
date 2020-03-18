b3ca4b3fe63ab8e9eea16387045213b58c82936f
yuriy-jk <complete@bk.ru>
2020-03-18 16:09:07 +0300

    django

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_hello
+sudo ln -sf /home/box/web/etc/unicorn.py /etc/gunicorn.d/unicorn.py
+gunicorn -c /etc/gunicorn.d/unicorn.py
