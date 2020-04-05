46f847162949279c99713b7fb4372b12792d21c8
yuriy-jk <complete@bk.ru>
2020-04-05 03:24:26 +0300

    terminal

--- terminal.sh
+++ terminal.sh
@@ -4,5 +4,5 @@ sudo apt install python3.5-dev
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo python3 -m pip install gunicorn
-sudo python3 -m install django==2.0
-sudo python3 -m pip install mysqlclientsu
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
